f2ddf5a0b1a1c2dbe70a72f79e6cbb6fb6321f6f
Jerry <506670809@qq.com>
2021-01-27 18:03:09 +0800

    添加项目文件。

---
+++ SqlSugarDemo/Controllers/StudentController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SqlSugar;
+using SqlSugarDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SqlSugarDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public StudentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/<StudentController>
+        [HttpGet]
+        public async Task<IEnumerable<Student>> Get()
+        {
+            var _context = GetInstance();
+            var list = await _context.Queryable<Student>().ToListAsync();
+            return list;
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StudentController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StudentController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StudentController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StudentController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+
+        private SqlSugarClient GetInstance()
+        {
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig
+            {
+                ConnectionString = _configuration.GetConnectionString("SqlServerConnection"),//连接符字串
+                DbType = DbType.SqlServer,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute//从特性读取主键自增信息
+            });
+
+            //添加Sql打印事件，开发中可以删掉这个代码
+            db.Aop.OnLogExecuting = (sql, pars) =>
+            {
+                Console.WriteLine(sql);
+            };
+            return db;
+        }
+    }
+}
---
+++ SqlSugarDemo/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqlSugarDemo.Models
+{
+    
+    [SugarTable("Student")]//当和数据库名称不一致时候可以设置别名
+    public class Student
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)] //通过特性设置主键和自增列 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        [SugarColumn(ColumnName ="Sex")]//数据库列名取自定义
+        public string Sex { get; set; }
+    }
+}
